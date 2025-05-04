a6bcad2bf67913d9b50d95f1c329e3d0b22b3a58
Martijn L. <martijn.libbrecht@gmail.com>
2025-05-04 18:10:10 +0200

    Changed final folder

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 echo "Building website into one file!"
 node minify_all_code.js
 echo "Brotli: compressing into index.html"
-rm index.html.br
 brotli index.html
 echo "Done!"
 echo "Build files: index.html & index.html.br"
 
 #Optional Move
+echo "Moved files to GitPages"
 mv index* ../kernelle.github.io/
